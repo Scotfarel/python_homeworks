4a3e8c6eea16d4de1e3252679695c47549a80d1a
Scotfarel <scotfarel@gmail.com>
2018-11-07 22:44:08 +0300

    based skeleton

--- task_queue/server.py
+++ task_queue/server.py
@@ -1,13 +1,125 @@
-
+import socket
 import argparse
+import uuid
+from collections import defaultdict
+
+
+class Task:
+    def __init__(self, length, data):
+        self._task_id = uuid.uuid4()
+        self._length = length
+        self._data = data
+        self._status = 'Free'
+
+    @property
+    def task_id(self):
+        return self._task_id
+
+
+class Command:
+    def __init__(self, command):
+        args_list = str(command).split(" ")
+        print(args_list)
+
+        self._method = args_list[0]
+        self._queue_name = args_list[1]
+        self._task_id = args_list[1]
+        self._task_length = args_list[2]
+        self._task_data = args_list[3]
+
+    @property
+    def method(self):
+        return self._method
+
+    @property
+    def queue_name(self):
+        return self._queue_name
+
+    @property
+    def task_length(self):
+        return self._task_length
+
+    @property
+    def task_data(self):
+        return self._task_data
+
+    @property
+    def task_id(self):
+        return self._task_id
 
 
 class TaskQueueServer:
     def __init__(self, ip, port, path, timeout):
-        pass
+        self._ip = ip
+        self._port = port
+        self._path = path
+        self._timeout = timeout
+        self._queues = defaultdict()
+
+    def apply_command_action(self, current_command):
+        t = "b'ADD"
+        if current_command.method == t:
+            self.add_task(current_command)
+        elif current_command.method == 'GET':
+            self.get_task(current_command)
+        elif current_command.method == 'ACK':
+            self.ack_task(current_command)
+        elif current_command.method == 'IN':
+            self.check_task_in_queue(current_command)
+        elif current_command.method == 'SAVE':
+            self.save_state()
+        else:
+            raise AttributeError
 
     def run(self):
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        connection.bind((self.ip, self.port))
+        connection.listen(1)
+        while True:
+            current_connection, address = connection.accept()
+            while True:
+                command = current_connection.recv(2048)
+                current_command = Command(command)
+                res = self.apply_command_action(current_command)
+                current_connection.send(res)
+
+    def add_task(self, current_command):
+        task = Task(current_command.task_length, current_command.task_data)
+        self.queues[current_command.queue_name].append(task)
+        return task.task_id
+
+    def get_task(self, current_command):
         pass
+
+    def ack_task(self, current_command):
+        pass
+
+    def check_task_in_queue(self, current_command):
+        pass
+
+    def save_state(self):
+        pass
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def timeout(self):
+        return self._timeout
+
+    @property
+    def queues(self):
+        return self._queues
 
 
 def parse_args():
@@ -17,7 +129,7 @@ def parse_args():
         action="store",
         dest="port",
         type=int,
-        default=8080,
+        default=5555,
         help='Server port')
     parser.add_argument(
         '-i',
